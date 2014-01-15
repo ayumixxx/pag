0f3e56bb5e9b758b7ee19b0ff72793ea6926e995
ayumi <ayumixxx@gmail.com>
2014-01-15 11:54:10 +0900

    tekitou

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pag::Application.routes.draw do
+  get "tops/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
